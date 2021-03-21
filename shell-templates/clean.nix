ff6340db6845f046f68cc3fab3a5f90969246778
Erin van der Veen <erin@erinvanderveen.nl>
2021-03-21 16:08:53 +0100

    Add Clean template

---
+++ shell-templates/clean.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+(pkgs.buildFHSUserEnv {
+  name = "clean-env";
+  multiPkgs = pkgs: with pkgs; [
+    pkg-config
+  ];
+  targetPkgs = pkgs: with pkgs; [
+    binutils-unwrapped
+    coreutils
+    gcc
+  ];
+}).env
