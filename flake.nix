8fc3254c089a9d3655caf98c641893da09a3c6ab
Erin van der Veen <erin@erinvanderveen.nl>
2022-05-11 14:48:51 +0200

    alacritty: switch from kitty

--- flake.nix
+++ flake.nix
@@ -186,7 +186,7 @@
           importables = rec {
             profiles = digga.lib.rakeLeaves ./users/profiles;
             suites = with profiles; rec {
-              base = [ core direnv nvim kitty bat skim helix exa ];
+              base = [ core direnv nvim alactritty bat skim helix exa ];
               development = [ git lazygit haskell ];
               desktop = [ gnome nextcloud desktop-packages ];
               work = [ tweag ];
