efa4c47271c390052c36cded51a64fab7faff2a3
Erin van der Veen <erin@erinvanderveen.nl>
2022-05-11 20:58:09 +0200

    zellij: pine rose theme

--- users/profiles/zellij.nix
+++ users/profiles/zellij.nix
@@ -2,5 +2,22 @@
 {
   programs.zellij = {
     enable = true;
+    settings = {
+      theme = "rose-pine";
+      themes.rose-pine = {
+        fg = 8;
+        bg = 0;
+        black = 234;
+        gray = 0;
+        red = 3;
+        green = 3;
+        yellow = 0;
+        blue = 0;
+        magenta = 0;
+        cyan = 0;
+        white = 5;
+        orange = 1;
+      };
+    };
   };
 }
