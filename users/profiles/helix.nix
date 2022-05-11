c9487f398ab089f84e5fdcfedceed0b6b1fddb7f
Erin van der Veen <erin@erinvanderveen.nl>
2022-05-11 14:47:20 +0200

    helix: have bar in insert mode

--- users/profiles/helix.nix
+++ users/profiles/helix.nix
@@ -6,6 +6,13 @@
       theme = "rose_pine";
       editor = {
         line-number = "relative";
+        cursor-shape = {
+          insert = "bar";
+        };
+        idle-timeout = 0;
+        lsp = {
+          display-messages = true;
+        };
       };
     };
   };
