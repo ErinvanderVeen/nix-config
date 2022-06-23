b5d9cfac9e2e997e7391ad4ff46148faf38d663f
Erin van der Veen <erin@erinvanderveen.nl>
2022-06-23 21:37:21 +0200

    mail: init with himalaya

--- flake.nix
+++ flake.nix
@@ -186,7 +186,7 @@
           importables = rec {
             profiles = digga.lib.rakeLeaves ./users/profiles;
             suites = with profiles; rec {
-              base = [ core direnv nvim zellij bat skim helix ];
+              base = [ core direnv nvim zellij bat skim helix mail ];
               development = [ git lazygit github ];
               desktop = [ sway gnome nextcloud desktop-packages discord alacritty ];
               work = [ tweag ];
---
+++ users/profiles/mail.nix
@@ -0,0 +1,49 @@
+{ pkgs, ... }:
+{
+  accounts.email.accounts."personal" = {
+    address = "erin@erinvanderveen.nl";
+    himalaya = {
+      enable = true;
+    };
+    imap = {
+      host = "imap.transip.email";
+      tls.enable = true;
+      port = 993;
+    };
+    primary = true;
+    realName = "Erin van der Veen";
+    smtp = {
+      host = "smtp.transip.email";
+      tls.enable = true;
+      port = 465;
+    };
+    userName = "erin@erinvanderveen.nl";
+    passwordCommand = "${pkgs.libsecret}/bin/secret-tool lookup account erin@erinvanderveen.nl";
+  };
+
+  programs.himalaya = {
+    enable = true;
+    settings = { };
+  };
+
+  programs.neovim.plugins = [
+    {
+      plugin = pkgs.vimPlugins.himalaya-vim;
+      config = ''
+        let g:himalaya_mailbox_picker = 'telescope'
+        let g:himalaya_telescope_preview_enabled = 0
+        lua <<EOF
+          local wk = require("which-key")
+          wk.register{
+            ["<leader>"] = {
+              m = {
+                name = "+mail",
+                m = { "<cmd>Himalaya<CR>", "Open Mailbox" },
+              },
+            },
+          }
+        EOF
+      '';
+    }
+  ];
+}
