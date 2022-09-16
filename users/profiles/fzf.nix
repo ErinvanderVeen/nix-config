1ad51d77cd102d379d4d4cddb1111b6154854058
Erin van der Veen <erin@erinvanderveen.nl>
2022-09-16 09:34:18 +0200

    fzf: disable fish integration

--- users/profiles/fzf.nix
+++ users/profiles/fzf.nix
@@ -3,6 +3,11 @@
   programs.fzf = {
     enable = true;
     enableBashIntegration = true;
-    enableFishIntegration = true;
+    # Fish integration is disabled in favour of the fzf fish plugin. As per this statement from that project's README:
+    # Note that the fzf utility has its own out-of-the-box fish package. What
+    # sets this package apart is that it has a couple more integrations, most
+    # notably tab completion, and will probably be updated more frequently. They
+    # are not compatible so either use one or the other.
+    #enableFishIntegration = true;
   };
 }
