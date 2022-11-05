97c59e561e9bc908ab4245396863bbd50d2ea141
Erin van der Veen <erin@erinvanderveen.nl>
2022-11-05 22:09:49 +0100

    erin: replace legacy rsa key

--- users/erin/default.nix
+++ users/erin/default.nix
@@ -10,9 +10,7 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "NetworkManager" ];
     openssh.authorizedKeys.keys = [
-      ''
-        ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3x2bjJ6q2XF5Po3sSoqC5C3AoiFR20cg+zuZh5QNuKHg1JeLm9270AsBkjJFU06AqVDQTHhrMxp/YEMOHqKgzujX6kp1+xmPaJgjuXh1a1yG8QM4EmBuYHJ4gk5MvRRMwKuqDFzOG0AmhjmBGNsfs8UkGyq92DGN7GQ+Zi3R6e6HW/Z4XeVMZG+WQscpJiiEntYrIN5oJ5H4CXem8IbLpCpplvF4zlb02D3BGa2BmlpCdvY1SxosfXBWP4jREiRBtJQtzP7T4144Gq1Psr5ge7kEH7qg1llTiuU75hEYsqPsL+5GBFIItS3czfg4ToMb/D2Vwuku0NbGh+uH9wUIthDmmOjwWHugmus0SlK39ZmOMi2QcEo0pQJhhNhUA07hcuSQ7IsqEeg+3hcUD5rzDKE5Gl7eRUi53OjBMCHFqJt3bNnYGv6F0v+V3uf2fPPWy/7AGVmUGwF937wa4Pw4w/bxjXYuN6BGmQuDENFm0aDRPJkt7F+GcPZjSxjBWKi9Qq+Cyph+c6lcbZJuw9WgKz2VW+xPMIZZ6QkWGvU+n763EVNuOszbpy0D92svyzfWBR3ZvbDAF/l/RnGAuMQs4nBbCzLYV1cDlxfyUH9TmQ6/xirG/xmK2gy0KTuZzpzk3h9N9kjBiGHcs2eHWjia0/9RwPH3gJKQUCeGQHZoMMw== erin@erinvanderveen.nl
-      ''
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF8EiA/6R8FrrXYbhV0eIWqSv5svnXbuDr4l/qgDUQcO erin@erinvanderveen.nl"
     ];
   };
 }
