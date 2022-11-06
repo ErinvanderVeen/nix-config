0112cdf4ec16c32412e654fff851757c9f01c998
Erin van der Veen <erin@erinvanderveen.nl>
2022-11-06 21:23:16 +0100

    taimi: add memtest

--- hosts/nixos/Taimi/default.nix
+++ hosts/nixos/Taimi/default.nix
@@ -7,8 +7,13 @@
   ++ suites.games
   ++ suites.development;
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      memtest86.enable = true;
+    };
+    efi.canTouchEfiVariables = true;
+  };
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
