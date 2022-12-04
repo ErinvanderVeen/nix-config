e067b5a56ac52d779e22ff60d063cd4e0a698864
Erin van der Veen <erin@erinvanderveen.nl>
2022-12-04 19:18:58 +0100

    skim: reverse sort; remove default options

--- users/profiles/skim.nix
+++ users/profiles/skim.nix
@@ -5,8 +5,7 @@
     enableBashIntegration = true;
     enableFishIntegration = true;
     changeDirWidgetOptions = [ "--preview 'exa -l {}'" ];
-    defaultOptions = [ "--preview 'bat {}'" ];
     fileWidgetOptions = [ "--preview 'bat {}'" ];
-    historyWidgetOptions = [ "--tac" "--exact" ];
+    historyWidgetOptions = [ "--exact" ];
   };
 }
