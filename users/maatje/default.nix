e54381cb9362690cd58492861d85d317888d97f8
Erin van der Veen <erin@erinvanderveen.nl>
2022-12-23 10:51:22 +0100

    maatje: change password

--- users/maatje/default.nix
+++ users/maatje/default.nix
@@ -1,11 +1,10 @@
-{ hmUsers, suites, ... }:
+{ hmUsers, suites, inputs, ... }:
 {
   home-manager.users = { inherit (hmUsers) maatje; };
 
   users.users.maatje = {
     name = "maatje";
     description = "Maatje";
-    hashedPassword = "$y$j9T$A4L2dC.8plzOSwqGEMBHe/$1VcvHokh.7jUR5LHVdCW3NOCKAIUgN1LfTxyBk3lxa6";
     home = "/home/maatje";
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "video" ];
