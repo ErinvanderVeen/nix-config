aa05085b8f7b84bb067bef550e281df72bf44254
Erin van der Veen <tybalt@erinvanderveen.nl>
2023-01-07 14:55:39 +0000

    transmission: enable rpc

--- profiles/transmission/default.nix
+++ profiles/transmission/default.nix
@@ -7,6 +7,10 @@
     settings = {
       download-dir = "/var/media/series";
       incomplete-dir = "/var/media/incomplete";
+      rpc-bind-address = "0.0.0.0";
+      rpc-whitelist-enabled = false;
+      rpc-host-whitelist-enabled = false;
     };
+    openRPCPort = true;
   };
 }
