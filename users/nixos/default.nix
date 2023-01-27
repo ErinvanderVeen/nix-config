caab443ff1f4de6822d77665596724c65cffb2f4
Erin van der Veen <erin@erinvanderveen.nl>
2023-01-27 21:23:17 +0100

    nixos: add ssh key; add to media group

--- users/nixos/default.nix
+++ users/nixos/default.nix
@@ -6,9 +6,10 @@
     description = "default";
     initialHashedPassword = "$y$j9T$YL05DpRpKY0L9JEbeUvvu/$zdCZDf9iDJ0B3T9n2y7fKoIyHQhhB981Dcdff19Kqf9";
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "wheel" "media" ];
     openssh.authorizedKeys.keys = [
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBD9ABNVaBX71Hb/u2IVOr9ZAAhnp7szmE3k6fX0FBPVTmoPMbSbewWdnsvi0syzIb+oWajU4ipHG5UZHkAMpIi8= erin@Gwen"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE+jQ+XfIiLyP9y4GPoPCVqwgry+v6pi/nAma3z6oRqA erin@Tequatl"
     ];
   };
 }
