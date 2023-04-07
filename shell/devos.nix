2b986a3e6bcb97256b3eabfc1b4b8f88d097d067
Erin van der Veen <erin@erinvanderveen.nl>
2023-04-07 20:47:09 +0200

    mdbook: remove

--- shell/devos.nix
+++ shell/devos.nix
@@ -1,10 +1,14 @@
-{ pkgs, extraModulesPath, inputs, lib, ... }:
+{ pkgs
+, extraModulesPath
+, inputs
+, lib
+, ...
+}:
 let
-
-  inherit (pkgs)
+  inherit
+    (pkgs)
     cachix
     editorconfig-checker
-    mdbook
     nixUnstable
     nixpkgs-fmt
     nvfetcher
@@ -15,8 +19,6 @@ let
   pkgWithCategory = category: package: { inherit package category; };
   devos = pkgWithCategory "devos";
   linter = pkgWithCategory "linter";
-  docs = pkgWithCategory "docs";
-
 in
 {
   _file = toString ./.;
@@ -24,26 +26,24 @@ in
   imports = [ "${extraModulesPath}/git/hooks.nix" ];
   git = { inherit hooks; };
 
-  commands = [
-    (devos nixUnstable)
+  commands =
+    [
+      (devos nixUnstable)
 
-    {
-      category = "devos";
-      name = nvfetcher.pname;
-      help = nvfetcher.meta.description;
-      command = "cd $PRJ_ROOT/pkgs; ${nvfetcher}/bin/nvfetcher -c ./sources.toml $@";
-    }
+      {
+        category = "devos";
+        name = nvfetcher.pname;
+        help = nvfetcher.meta.description;
+        command = "cd $PRJ_ROOT/pkgs; ${nvfetcher}/bin/nvfetcher -c ./sources.toml $@";
+      }
 
-    (linter nixpkgs-fmt)
-    (linter editorconfig-checker)
-
-    (docs mdbook)
-  ]
-  ++ lib.optionals (!pkgs.stdenv.buildPlatform.isi686) [
-    (devos cachix)
-  ]
-  ++ lib.optionals (pkgs.stdenv.hostPlatform.isLinux && !pkgs.stdenv.buildPlatform.isDarwin) [
-    (devos inputs.nixos-generators.defaultPackage.${pkgs.system})
-  ]
-  ;
+      (linter nixpkgs-fmt)
+      (linter editorconfig-checker)
+    ]
+    ++ lib.optionals (!pkgs.stdenv.buildPlatform.isi686) [
+      (devos cachix)
+    ]
+    ++ lib.optionals (pkgs.stdenv.hostPlatform.isLinux && !pkgs.stdenv.buildPlatform.isDarwin) [
+      (devos inputs.nixos-generators.defaultPackage.${pkgs.system})
+    ];
 }
