e2385c6e950bd645d618713a4d063e6870326ede
Erin van der Veen <erin@erinvanderveen.nl>
2023-04-29 23:48:18 +0200

    overrides: override 2 kodiPackages

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -17,6 +17,70 @@ channels: final: prev: {
     steam
     ;
 
+  kodiPackages =
+    prev.kodiPackages
+    // rec
+    {
+      infotagger = prev.callPackage
+        ({ lib
+         , buildKodiAddon
+         , fetchFromGitHub
+         , addonUpdateScript
+         ,
+         }:
+          buildKodiAddon rec {
+            pname = "infotagger";
+            namespace = "script.module.infotagger";
+            version = "0.0.7";
+
+            src = fetchFromGitHub {
+              owner = "jurialmunkey";
+              repo = namespace;
+              rev = "v${version}";
+              hash = "sha256-Us7ud0QORGn+ALB4uyISekp0kUYY8nN8uFNg8MlxEB0=";
+            };
+
+            passthru = {
+              # Unusual Python path.
+              pythonPath = "resources/modules";
+            };
+          })
+        { };
+
+      youtube = prev.callPackage
+        ({ lib
+         , buildKodiAddon
+         , fetchzip
+         , addonUpdateScript
+         , six
+         , requests
+         , inputstreamhelper
+         ,
+         }:
+          buildKodiAddon rec {
+            pname = "youtube";
+            namespace = "plugin.video.youtube";
+            version = "7.0.1";
+
+            src = fetchzip {
+              url = "https://mirrors.kodi.tv/addons/nexus/${namespace}/${namespace}-${version}.zip";
+              sha256 = "sha256-Wdju7d2kFX0V1J1TB75qEVq0UWN2xYYFNlD8UTt1New=";
+            };
+
+            propagatedBuildInputs = [
+              six
+              requests
+              infotagger
+              inputstreamhelper
+            ];
+
+            passthru = {
+              pythonPath = "resources/lib";
+            };
+          })
+        { };
+    };
+
   lib =
     prev.lib
     // rec {
